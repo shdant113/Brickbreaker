68203df705915407f8587d58e4c00c034ccc19a3
Spencer Dant <sdant@Spencers-MacBook-Pro.local>
2019-01-07 13:17:50 -0600

    boundaries set, trail effect on ball

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 // console.log('thing')
 
-// LEVEL 1 JAVASCRIPT
+// LEVEL 1 SCRIPT
 
 let level1Canvas = document.getElementById('level1-canvas');
 const ctx = level1Canvas.getContext('2d');
@@ -46,25 +46,24 @@ const ball = {
 	createBall() {
 		ctx.beginPath();
 		ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+		ctx.closePath();
 		ctx.fillStyle = this.color;
 		ctx.fill();
 	},
+	// animate() {
+	// 	ctx.clearRect(0, 0, w, h);
+	// 	this.createBall();
+	// 	this.x += this.vx;
+	// 	this.y += this.vy;
+	// 	if (this.x + this.vx > w || this.x + this.vx < 0) {
+	// 		this.vx = -this.vx
+	// 	}
+	// 	if (this.y + this.vy > h || this.y + this.vy < 0) {
+	// 		this.vy = -this.vy
+	// 	}
+	// 	amt = window.requestAnimationFrame(this.animate);
+	// }
 }
-
-function animate() {
-	ctx.clearRect(0, 0, w, h);
-	ball.createBall();
-	ball.x += ball.vx;
-	ball.y += ball.vy;
-	if (ball.y + ball.vy > h || ball.y + ball.vy < 0) {
-		ball.vy = -ball.vy
-	}
-	if (ball.x + ball.xy > w || ball.x + ball.xy < 0) {
-		ball.xy = -ball.xy
-	}
-	amt = window.requestAnimationFrame(animate);
-}
-
 
 const paddle = {
 	x: 400,
@@ -72,23 +71,23 @@ const paddle = {
 	color: 'red',
 	createPaddle() {
 		ctx.fillStyle = this.color;
-		ctx.strokeStyle = 'white';
+		ctx.strokeStyle = 'black';
 		ctx.beginPath();
-		ctx.rect(this.x, this.y, 100, 30);
+		ctx.rect(this.x, this.y, 150, 30);
 		ctx.fill();
 		ctx.stroke();
 	},
 	movePaddleLeft() {
 		this.createPaddle();
-		this.x-=40;
-		while (this.x < 0) {
+		this.x-=80;
+		while (this.x < 1) {
 			this.x+=1;
 		}
 	},
 	movePaddleRight() {
 		this.createPaddle();
-		this.x+=40;
-		while (this.x > 800) {
+		this.x+=80;
+		while (this.x > 749) {
 			this.x-=1;
 		}
 	}
@@ -112,9 +111,24 @@ const brick = {
 	}
 }
 
-
-
-
+function animate() {
+	// ctx.clearRect(0, 0, w, h);
+	ctx.fillStyle = 'rgba(255, 255, 255, 0.5)';
+  	ctx.beginPath();
+  	ctx.rect(0, 0, w, h);
+  	ctx.fill();
+	ball.createBall();
+	paddle.createPaddle();
+	ball.x += ball.vx;
+	ball.y += ball.vy;
+	if (ball.y + ball.vy > h || ball.y + ball.vy < 0) {
+		ball.vy = -ball.vy
+	}
+	if (ball.x + ball.vx > w || ball.x + ball.vx < 0) {
+		ball.vx = -ball.vx
+	}
+	amt = window.requestAnimationFrame(animate);
+} 
 
 
 // EVENT LISTENERS
@@ -135,7 +149,9 @@ document.getElementById('start-game').addEventListener('click', (e) => {
 	ball.createBall();
 	paddle.createPaddle();
 	brick.createBricks();
+	ctx.save();
 	animate();
+	ctx.restore();
 });
 
 
