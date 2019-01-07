e756e0bc853535d7cb641bef1a7a3b8e94c93591
Spencer Dant <sdant@Spencers-MacBook-Pro.local>
2019-01-07 09:50:34 -0600

    initial basic design concept

---
+++ js/app.js
@@ -0,0 +1,76 @@
+console.log('thing')
+
+let level1Canvas = document.getElementById('level1-canvas');
+const ctx = level1Canvas.getContext('2d');
+
+// CLASSES
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// OBJECTS
+
+const ball = {
+
+}
+
+const paddle = {
+	
+}
+
+const brick = {
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// EVENT LISTENERS
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
